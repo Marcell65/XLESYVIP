2bc2042680dcdd2471c686adc5879e713ace04d7
Gxyenn <GxyennDEV@gmail.com>
2025-05-09 03:48:59 +0700

    Add files via upload

--- XlesySet.js
+++ XlesySet.js
@@ -1,5 +1,6 @@
 // config.js
 const XlesySet = {
+    // Info
     owner: ['6283877636168'],
     packname: 'Xlesy',
     author: 'Gxyenn',
@@ -7,11 +8,19 @@ const XlesySet = {
     listprefix: ['.'],
     listv: ['❀','✿','⚘','々'],
     number_bot: "", // Kalo pake panel bisa masukin nomor di sini, jika belum ambil session. Format: '628xx'
+    // Payment
     dana: ['083877636168'],
     gopay: ['083877636168'],
     ovo: ['kosong'],
     pulsa: ['083877636168'],
     qris: ['kosong'],
+    // Cpanel 
+    egg = "15", // Isi id egg
+    nestid = "5", // Isi id nest
+    loc = "1", // Isi id location
+    domain = "_", // isi domai lu di sini 
+    apikey = "_", // Isi api ptla
+    capikey = "_", // Isi api ptlc
 };
 
 module.exports = XlesySet
